a23cc6d4c3837686dbd8a1d04c46c225c346627c
izunarin <112565053+izunarin@users.noreply.github.com>
2022-10-25 15:12:08 +0000

    [add]4-10エラーのため未完成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,29 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = aurrent_user.id
+    @post_image.save
+    redirect_to post_image_path
   end
 
   def index
+    @post_images = post_image.all
   end
 
   def show
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
